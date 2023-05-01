905a95344b7cfb1dc0e6ba537b542aa39aa95d53
Rajat <rajatdhoot123@gmail.com>
2023-05-01 21:58:48 +0530

    code added

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -1,17 +1,38 @@
 import ChatMessage from "@/components/ChatMessage";
+import { useSocket } from "@/context/SocketContext";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { FaRobot, FaPaperPlane } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
 
 const LEFT_ICONS = [{ label: "Agent" }];
 
 const App = () => {
+  const [state, setState] = useState([]);
   const supabaseClient = useSupabaseClient();
   const router = useRouter();
+  const socket = useSocket();
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("answer", (answer) => {});
+    }
+  }, [socket]);
 
   const handleSubmit = (event) => {
+    const formData = new FormData(event.currentTarget);
+    let data = {};
+    for (let [key, value] of formData.entries()) {
+      data[key] = value;
+      data.id = uuidv4();
+      data.type = "question";
+    }
+    socket.emit("question", data);
+    setState((prev) => [...prev, data]);
     event.preventDefault();
   };
+
   return (
     <div className="grid grid-cols-12 chat-bg h-screen">
       <div className="col-span-2 p-5 h-full bg-[#202123]">
@@ -35,248 +56,21 @@ const App = () => {
       <div className="col-span-10 overflow-y-scroll">
         <div className="flex flex-col justify-between h-full gap-5">
           <div className="flex-1 h-full overflow-y-scroll">
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
-            <ChatMessage
-              message={{
-                user: { name: "Rajats", avatarUrl: "" },
-                text: "Hello",
-              }}
-              isCurrentUser
-            />
-            <ChatMessage
-              isCurrentUser={false}
-              message={{
-                user: {
-                  name: "Rajats",
-                  avatarUrl: <FaRobot size="36px" />,
-                },
-                text: "Hello",
-              }}
-            />
+            {state.map((el) => (
+              <ChatMessage
+                key={el.id}
+                message={{
+                  user: { name: "Rajats", avatarUrl: "" },
+                  text: el.text,
+                }}
+                isCurrentUser={el.type === "question"}
+              />
+            ))}
           </div>
           <div className="px-44 mb-12">
             <form onSubmit={handleSubmit} className="flex">
               <input
+                name="text"
                 placeholder="Type here"
                 className="w-full rounded-l-md px-5 py-2 focus:outline-none focus:border-blue-200 border"
               />
