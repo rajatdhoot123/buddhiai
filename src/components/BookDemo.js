9a1706a3924a0968de6dfcdb97417b1cfbfe4bf0
Rajat <rajatdhoot123@gmail.com>
2023-05-01 11:02:56 +0530

    Book Demo added

--- src/components/BookDemo.js
+++ src/components/BookDemo.js
@@ -1,12 +1,23 @@
-const BookDemo = () => {
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+
+export default function Calander() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi();
+      cal("ui", {
+        theme: "light",
+        styles: { branding: { brandColor: "#eaade1" } },
+        hideEventTypeDetails: false,
+      });
+    })();
+  }, []);
   return (
     <button
       data-cal-link="kwiktwik/30min"
       className="px-5 py-2 rounded-md bg-[#293FCC] text-white"
     >
-      Request Demo
+      Book Demo
     </button>
   );
-};
-
-export default BookDemo;
+}
