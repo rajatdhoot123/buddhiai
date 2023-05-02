d8ec3dbc0564ad1f2f48b108f1d1f192e1274177
Rajat <rajatdhoot123@gmail.com>
2023-05-02 17:50:18 +0530

    Added bots

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -8,12 +8,11 @@ import { useRef, useState } from "react";
 import { FaEllipsisH, FaPaperPlane } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
 
-const LEFT_ICONS = [{ label: "Agent" }];
-
 const App = () => {
   const [state, setState] = useState([]);
   const user = useUser();
   const lastMessageRef = useRef(null);
+
   const handleSubmit = async (event) => {
     const formData = new FormData(event.currentTarget);
     let payload = {};
@@ -51,23 +50,18 @@ const App = () => {
   };
 
   return (
-    <div className="grid grid-cols-12 chat-bg h-screen">
-      <div className="col-span-2 p-5 h-full bg-[#202123]">
-        <div className="flex flex-col justify-between h-full">
-          <ul>
-            {LEFT_ICONS.map(({ label }) => (
-              <li className="text-white" key={label}>
-                {label}
-              </li>
-            ))}
-          </ul>
-          <ThreeDotMenu>
-            <div className="flex items-center justify-between">
-              <div className="w-3/4 truncate">{user?.email}</div>
-              <FaEllipsisH className="w-1/4" />
-            </div>
-          </ThreeDotMenu>
-        </div>
+    <div className="flex flex-col justify-between h-full gap-5">
+      <div className="flex-1 h-full overflow-y-scroll">
+        {state.map((el) => (
+          <ChatMessage
+            key={el.id}
+            message={{
+              user: { name: "Rajats", avatarUrl: "" },
+              text: el.text,
+            }}
+            isCurrentUser={el.type === "question"}
+          />
+        ))}
       </div>
       <div className="col-span-10 overflow-y-scroll">
         <div className="flex flex-col justify-between h-full gap-5">
