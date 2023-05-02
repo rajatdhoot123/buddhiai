3fe08f4c71e0150d83656ba4bda5d9dcf369650b
Rajat <rajatdhoot123@gmail.com>
2023-05-02 18:56:31 +0530

    Fixes with thinking message

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -4,7 +4,7 @@ import ThreeDotMenu from "@/components/ThreeDotMenu";
 // import { useSocket } from "@/context/SocketContext";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { FaEllipsisH, FaPaperPlane } from "react-icons/fa";
 import { v4 as uuidv4 } from "uuid";
 
@@ -13,8 +13,7 @@ const LEFT_ICONS = [{ label: "Agent" }];
 const App = () => {
   const [state, setState] = useState([]);
   const user = useUser();
-  // const socket = useSocket();
-
+  const lastMessageRef = useRef(null);
   const handleSubmit = async (event) => {
     const formData = new FormData(event.currentTarget);
     let payload = {};
@@ -29,7 +28,7 @@ const App = () => {
       setState((prev) => [
         ...prev,
         {
-          type: "question",
+          type: "thinking",
           id: uuidv4(),
           text: "Hang tight, we're brainstorming to find the perfect solution to your question!",
         },
@@ -46,6 +45,8 @@ const App = () => {
       ]);
     } catch (err) {
       console.log(err);
+    } finally {
+      lastMessageRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -71,16 +72,33 @@ const App = () => {
       <div className="col-span-10 overflow-y-scroll">
         <div className="flex flex-col justify-between h-full gap-5">
           <div className="flex-1 h-full overflow-y-scroll">
-            {state.map((el) => (
-              <ChatMessage
-                key={el.id}
-                message={{
-                  user: { name: "Rajats", avatarUrl: "" },
-                  text: el.text,
-                }}
-                isCurrentUser={el.type === "question"}
-              />
-            ))}
+            {state.map((el) =>
+              el.type === "thinking" ? (
+                <div key={el.id}>
+                  <div className="flex">
+                    <div className="flex px-44 space-x-2 items-center">
+                      <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
+                      <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
+                      <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
+                      <div className="text-white ml-2">{el.text}</div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <ChatMessage
+                  key={el.id}
+                  message={{
+                    user: { name: "Rajats", avatarUrl: "" },
+                    text: el.text,
+                  }}
+                  isCurrentUser={el.type === "question"}
+                />
+              )
+            )}
+            <div
+              style={{ float: "left", clear: "both" }}
+              ref={lastMessageRef}
+            ></div>
           </div>
           <div className="px-44 mb-12">
             <form onSubmit={handleSubmit} className="flex">
