99e48403a7c2c87d2023fe0e504da0cb171e211c
Rajat <rajatdhoot123@gmail.com>
2023-05-02 15:03:37 +0530

    Ai Bot Refined

---
+++ src/hooks/clickOutside.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+
+const useClickOutside = (ref, onClickOutside) => {
+  useEffect(() => {
+    /**
+     * Invoke Function onClick outside of element
+     */
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClickOutside();
+      }
+    }
+    // Bind
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // dispose
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, onClickOutside]);
+
+  return null;
+};
+
+export default useClickOutside;
