4f770712b5755b7a25216d34ab149c21e68c2234
Rajat <rajatdhoot123@gmail.com>
2023-05-03 15:01:48 +0530

    fix alignment

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -14,6 +14,7 @@ const App = () => {
     if (!activeFile) {
       if (typeof alert !== "undefined") {
         alert("Please select file to chat");
+        return;
       }
     }
     const formData = new FormData(event.currentTarget);
@@ -57,61 +58,45 @@ const App = () => {
   return (
     <div className="flex flex-col justify-between h-full gap-5">
       <div className="flex-1 h-full overflow-y-scroll">
-        {state.map((el) => (
-          <ChatMessage
-            key={el.id}
-            message={{
-              user: { name: "Rajats", avatarUrl: "" },
-              text: el.text,
-            }}
-            isCurrentUser={el.type === "question"}
-          />
-        ))}
-      </div>
-      <div className="col-span-10 overflow-y-scroll">
-        <div className="flex flex-col justify-between h-full gap-5">
-          <div className="flex-1 h-full overflow-y-scroll">
-            {state.map((el) =>
-              el.type === "thinking" ? (
-                <div key={el.id}>
-                  <div className="flex">
-                    <div className="flex px-44 space-x-2 items-center">
-                      <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
-                      <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
-                      <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
-                      <div className="text-white ml-2">{el.text}</div>
-                    </div>
-                  </div>
+        {state.map((el) =>
+          el.type === "thinking" ? (
+            <div key={el.id}>
+              <div className="flex">
+                <div className="flex px-44 space-x-2 items-center">
+                  <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
+                  <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
+                  <div className="flex-shrink-0 w-2 h-2 rounded-full animate-pulse bg-white"></div>
+                  <div className="text-white ml-2">{el.text}</div>
                 </div>
-              ) : (
-                <ChatMessage
-                  key={el.id}
-                  message={{
-                    user: { name: "Rajats", avatarUrl: "" },
-                    text: el.text,
-                  }}
-                  isCurrentUser={el.type === "question"}
-                />
-              )
-            )}
-            <div
-              style={{ float: "left", clear: "both" }}
-              ref={lastMessageRef}
-            ></div>
-          </div>
-          <div className="px-44 mb-12">
-            <form onSubmit={handleSubmit} className="flex">
-              <input
-                name="text"
-                placeholder="Type here"
-                className="w-full rounded-l-md px-5 py-2 focus:outline-none focus:border-blue-200 border"
-              />
-              <button type="submit" className="bg-white px-5 rounded-r-md">
-                <FaPaperPlane />
-              </button>
-            </form>
-          </div>
-        </div>
+              </div>
+            </div>
+          ) : (
+            <ChatMessage
+              key={el.id}
+              message={{
+                user: { name: "Rajats", avatarUrl: "" },
+                text: el.text,
+              }}
+              isCurrentUser={el.type === "question"}
+            />
+          )
+        )}
+        <div
+          style={{ float: "left", clear: "both" }}
+          ref={lastMessageRef}
+        ></div>
+      </div>
+      <div className="px-44 mb-12">
+        <form onSubmit={handleSubmit} className="flex">
+          <input
+            name="text"
+            placeholder="Type here"
+            className="w-full rounded-l-md px-5 py-2 focus:outline-none focus:border-blue-200 border"
+          />
+          <button type="submit" className="bg-white px-5 rounded-r-md">
+            <FaPaperPlane />
+          </button>
+        </form>
       </div>
     </div>
   );
