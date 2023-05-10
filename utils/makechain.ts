022a746bb5835d8f51757c5b23fdfaa9c7f9dd2b
Rajat <rajatdhoot123@gmail.com>
2023-05-10 09:49:20 +0530

    Create Prompt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,36 +1,9 @@
 import { OpenAI } from "langchain/llms/openai";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { CONDENSE_PROMPT, QA_PROMPT } from "@/constant/prompt";
 
-const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:`;
-
-const QA_SHOPPING_ASSITANCE = `You are a friendly, conversational retail shopping assistant. Use the following context including product names, descriptions, and keywords to show the shopper whats available with link of product, help find what they want, and answer any questions.
- It's ok if you don't know the answer.
-
-{context}
-
-Question: {question}
-Helpful answer in markdown:`;
-
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
-{context}
-
-Question: {question}
-Helpful answer in markdown:`;
-
-const QA_PROMPT_MAPPER: { [key: string]: string } = {
-  shopping_agent: QA_SHOPPING_ASSITANCE,
-};
-
-export const makeChain = (vectorstore: HNSWLib, agentType: string) => {
+export const makeChain = (vectorstore: HNSWLib, prompt: string) => {
   const model = new OpenAI({
     temperature: 0, // increase temepreature to get more creative answers
     modelName: "gpt-3.5-turbo", //change this to gpt-4 if you have access
@@ -40,7 +13,7 @@ export const makeChain = (vectorstore: HNSWLib, agentType: string) => {
     model,
     vectorstore.asRetriever(),
     {
-      qaTemplate: QA_PROMPT_MAPPER[agentType] || QA_PROMPT,
+      qaTemplate: prompt || QA_PROMPT,
       questionGeneratorTemplate: CONDENSE_PROMPT,
       returnSourceDocuments: true, //The number of source documents returned is 4 by default
     }
