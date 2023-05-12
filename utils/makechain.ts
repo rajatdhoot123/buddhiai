d39dfbd6b39c3d1bc6079f36a9d4edfe52cb5b14
Rajat <rajatdhoot123@gmail.com>
2023-05-12 21:47:56 +0530

    Add Prompt console

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,7 +1,7 @@
 import { OpenAI } from "langchain/llms/openai";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { ConversationalRetrievalQAChain } from "langchain/chains";
-import { CONDENSE_PROMPT, QA_PROMPT } from "@/constant/prompt";
+import { CONDENSE_PROMPT, PROMPT_SUFFIX, QA_PROMPT } from "@/constant/prompt";
 
 export const makeChain = (vectorstore: HNSWLib, prompt: string) => {
   const model = new OpenAI({
@@ -9,11 +9,12 @@ export const makeChain = (vectorstore: HNSWLib, prompt: string) => {
     modelName: "gpt-3.5-turbo", //change this to gpt-4 if you have access
   });
 
+  console.log(prompt || QA_PROMPT + PROMPT_SUFFIX, "PROMPT");
   const chain = ConversationalRetrievalQAChain.fromLLM(
     model,
     vectorstore.asRetriever(),
     {
-      qaTemplate: prompt || QA_PROMPT,
+      qaTemplate: prompt || QA_PROMPT + PROMPT_SUFFIX,
       questionGeneratorTemplate: CONDENSE_PROMPT,
       returnSourceDocuments: true, //The number of source documents returned is 4 by default
     }
