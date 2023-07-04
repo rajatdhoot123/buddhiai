63567e0db9d3291bbfaf7b886b009a53b990d615
Rajat <rajatdhoot123@gmail.com>
2023-07-04 12:36:05 +0530

    Pricing updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import BookDemo from "../components/BookDemo";
 import Layout from "../layout/public";
 import Image from "next/image";
@@ -21,7 +22,12 @@ const Landing = () => (
     </div>
     <div className="md:min-h-[620px] h-[280px] flex justify-center items-center relative">
       <div className="h-[250px] md:h-[600px] w-full absolute md:-top-36 -top-12">
-        <Image alt="Buddhi AI" className="object-contain" src="/hero.png" fill />
+        <Image
+          alt="Buddhi AI"
+          className="object-contain"
+          src="/hero.png"
+          fill
+        />
       </div>
     </div>
     <section className="bg-[#0D0C16] ">
@@ -31,8 +37,8 @@ const Landing = () => (
             BuddhiAi: Your Intelligent Companion for the Digital World
           </h2>
           <p className="text-white text-opacity-75 sm:text-xl ">
-            Navigate the digital world with ease - BuddhiAi guide is always
-            at your service!
+            Navigate the digital world with ease - BuddhiAi guide is always at
+            your service!
           </p>
         </div>
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
@@ -72,6 +78,275 @@ const Landing = () => (
               <p className="text-white text-opacity-75">{description}</p>
             </div>
           ))}
+        </div>
+      </div>
+    </section>
+    <section class="bg-[#1F222C] ">
+      <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-white ">
+            Designed for business teams like yours
+          </h2>
+          <p class="mb-5 font-light text-gray-300 sm:text-xl ">
+            Here at Flowbite we focus on markets where technology, innovation,
+            and capital can unlock long-term value and drive economic growth.
+          </p>
+        </div>
+        <div class="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0 p-5">
+          <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow  xl:p-8  ">
+            <h3 class="mb-4 text-2xl font-semibold">Starter</h3>
+            <p class="font-light text-gray-500 sm:text-lg ">
+              Best option for personal use & for your next project.
+            </p>
+            <div class="flex justify-center items-baseline my-8">
+              <span class="mr-2 text-5xl font-extrabold">$9</span>
+              <span class="text-gray-500 ">/month</span>
+            </div>
+
+            <ul role="list" class="mb-8 space-y-4 text-left">
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span>5mb storage</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span>Chat with PDF</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>100 messages / month</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Free updates</span>
+              </li>
+            </ul>
+            <Link
+              href="/contact-us"
+              class="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+            >
+              Buy Now
+            </Link>
+          </div>
+
+          <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow  xl:p-8">
+            <h3 class="mb-4 text-2xl font-semibold">Company</h3>
+            <p class="font-light text-gray-500 sm:text-lg ">
+              Relevant for multiple users, extended & premium support.
+            </p>
+            <div class="flex justify-center items-baseline my-8">
+              <span class="mr-2 text-5xl font-extrabold">$19</span>
+              <span class="text-gray-500 ">/month</span>
+            </div>
+
+            <ul role="list" class="mb-8 space-y-4 text-left">
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span>100mb storage</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span>Chat with multiple documents (PDF, CSV, Text)</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>1000 messages / month</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Free updates</span>
+              </li>
+            </ul>
+            <Link
+              href="/contact-us"
+              class="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+            >
+              Buy Now
+            </Link>
+          </div>
+
+          <div class="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow  xl:p-8  ">
+            <h3 class="mb-4 text-2xl font-semibold">Enterprise</h3>
+            <p class="font-light text-gray-500 sm:text-lg ">
+              Best for large scale uses and extended redistribution rights.
+            </p>
+            <div class="flex justify-center items-baseline my-8">
+              <span class="mr-2 text-5xl font-extrabold">Custom</span>
+              {/* <span class="text-gray-500 ">/month</span> */}
+            </div>
+
+            <ul role="list" class="mb-8 space-y-4 text-left">
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Unlimited Storage</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Unlimited Messages</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Generate summary for conversation</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Premium support</span>
+              </li>
+              <li class="flex items-center space-x-3">
+                <svg
+                  class="flex-shrink-0 w-5 h-5 text-green-500 "
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <span>Free updates</span>
+              </li>
+            </ul>
+            <Link
+              href="/contact-us"
+              class="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+            >
+              Buy Now
+            </Link>
+          </div>
         </div>
       </div>
     </section>
